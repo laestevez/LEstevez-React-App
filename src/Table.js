6066fda25f9247ab948430d3ef13f89feafcf89f
laestevez <luisangel.e10@gmail.com>
2023-01-15 21:26:16 -0800

    added delete button

--- src/Table.js
+++ src/Table.js
@@ -1,40 +1,44 @@
 import React from 'react'
 
-function TableHeader()  {
-    return (
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
-    );
-  }
+function TableHeader() {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
+  );
+}
 
 function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
-        <td>{row.name}</td>         
+        <td>{row.name}</td>
         <td>{row.job}</td>
       </tr>
     );
   }
   );
   return (
-      <tbody>
-        {rows}
-      </tbody>
-    );
-  }
+    <tr key={index}>
+      <td>{row.name}</td>
+      <td>{row.job}</td>
+      <td>
+        <button onClick={() => props.removeCharacter(index)}>Delete</button>
+      </td>
+    </tr>
+  );
+}
 
 function Table(props) {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={props.characterData} />
-      </table>
-    );
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
+    </table>
+  );
 }
 
 export default Table;
