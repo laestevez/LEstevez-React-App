9a1692f79a5570feb81cec6c643f9abaa279c199
laestevez <luisangel.e10@gmail.com>
2023-01-28 15:28:58 -0800

    database implementation

---
+++ backend/models/user-services.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+const userModel = require("./user");
+mongoose.set("debug", true);
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/users", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((error) => console.log(error));
+
+async function getUsers(name, job) {
+  let result;
+  let result2;
+  if (name === undefined && job === undefined) {
+    result = await userModel.find();
+  } else if (name && !job) {
+    result = await findUserByName(name);
+  } else if (job && !name) {
+    result = await findUserByJob(job);
+  } else {
+    result = await findUserByName(name);
+    result2 = await findUserByJob(job);
+    result = result.concat(result2)
+  }
+  return result;
+}
+
+async function findUserById(id) {
+  try {
+    return await userModel.findById(id);
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
+}
+
+async function deleteUserById(id) {
+  try {
+    return await userModel.findByIdAndDelete(id);
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
+}
+
+async function addUser(user) {
+  try {
+    const userToAdd = new userModel(user);
+    const savedUser = await userToAdd.save();
+    return savedUser;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+async function findUserByName(name) {
+  return await userModel.find({ name: name });
+}
+
+async function findUserByJob(job) {
+  return await userModel.find({ job: job });
+}
+
+exports.getUsers = getUsers;
+exports.findUserById = findUserById;
+exports.deleteUserById = deleteUserById;
+exports.addUser = addUser;
--- frontend/src/MyApp.js
+++ frontend/src/MyApp.js
@@ -17,7 +17,7 @@ function MyApp() {
     const updated = characters.filter((character, i) => {
       return i !== index
     });
-    makeDeleteCall(characters.at(index).id).then(result => {
+    makeDeleteCall(characters.at(index)._id).then(result => {
       if (result && result.status === 204)
         setCharacters(updated);
     });
